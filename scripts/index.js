3d375b45a2885b47e49eaf93e5310518306b33ca
Eugenius2902 <eugen.bogdanova@yandex.ru>
2024-08-22 16:58:16 +0300

    index.js for review

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,8 @@ function createCard (name, link, deleteCard) {
     const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
         placesItem.querySelector('.card__title').textContent = name;
         placesItem.querySelector('.card__image').src = link;
-    const deleteButton = cardTemplate.querySelector('.card__delete-button');
+        placesList.append(placesItem);
+    const deleteButton = placesItem.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', deleteCard);
     return placesItem;
 };
@@ -17,12 +18,11 @@ function createCard (name, link, deleteCard) {
 // @todo: Функция удаления карточки
 function deleteCard (evt) {
     const eventTarget = evt.target;
-    const placesItem = eventTarget.closest('.places__item');
-    placesItem.remove();
+    const card = eventTarget.closest('.places__item');
+    card.remove();
 };
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((el) => {
     placesList.append(createCard(el.name, el.link, deleteCard));
 });
-
